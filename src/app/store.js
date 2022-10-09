8ff148681d7ce4844bafd772e91cd14f9fce6481
Stanley Mair <stanleygmair@gmail.com>
2022-10-10 05:11:23 +1300

    Finished creating the topics slice and configured to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { addTopicReducer } from "../features/topics/topicsSlice";
 
 export default configureStore({
-  reducer: {},
+	reducer: {
+		addTopic: addTopicReducer,
+	},
 });
--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,24 +1,26 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const options = {
-    name: 'topics',
-    initialState: {
-        topics: {}
-    },
-    reducers: {
-        addTopic: (state, action) => {
-                state.topics.id: {
-                    id: action.payload.id,
-                    name: action.payload.name,
-                    icon: action.paylaod.icon,
-                    quizIds: [],
-                }
-            }
+  name: "topics",
+  initialState: {
+    topics: {}
+  },
+  reducers: {
+    addTopic: (state, action) => {
+      return {
+        ...state,
+        [action.payload.id]: {
+          id: action.payload.id,
+          name: action.payload.name,
+          icon: action.payload.icon,
+          quizIds: []
         }
+      };
     }
-
+  }
+};
 
 const topicsSlice = createSlice(options);
 
-export {addTopic} = topicsSlice.actions;
+export const { addTopicReducer } = topicsSlice.actions;
 export default topicsSlice.reducer;
